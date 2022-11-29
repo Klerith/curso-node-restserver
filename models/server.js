7bdbe3caed3867ef83af9e8fb357d126a1ebc525
Fernando <fernando.herrera85@gmail.com>
2022-11-29 12:35:51 -0500

    Default port updated

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
 
         // Middlewares
